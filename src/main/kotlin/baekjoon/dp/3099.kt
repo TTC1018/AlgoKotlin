fec31ef0325f0c6f6ce4eb5d36c8e389ec3769e8
DongHyeon Park <paramore@kakao.com>
2023-10-30 15:40:31 +0900

    EDIT: 백준 DP 3099 주석추가

--- src/main/kotlin/baekjoon/dp/3099.kt
+++ src/main/kotlin/baekjoon/dp/3099.kt
@@ -6,6 +6,7 @@ class `3099` {
     fun solution() = with(System.`in`.bufferedReader()) {
         val S = readLine()
         // 경우의 수 -> SET이냐 NEXT냐 WRITE냐
+        // S[i]까지 왔는데 현재 메모리에 alpha가 저장돼있는 경우의 횟수
         val dp = List(S.length) { IntArray('Z' - 'A' + 1) { S.length * 2 } }.apply {
             this[0][S.first().toIdx()] = 2 // SET - WRITE
         }
@@ -14,7 +15,7 @@ class `3099` {
                 if (dp[i - 1][alpha.toIdx()] != S.length * 2) {
                     if (S[i] == alpha) { // WRITE만 하면 됨
                         dp[i][alpha.toIdx()] = minOf(dp[i][alpha.toIdx()], dp[i - 1][alpha.toIdx()] + 1)
-                    } else {
+                    } else { // NEXT-WRITE OR SET-WRITE
                         dp[i][S[i].toIdx()] = minOf(dp[i][S[i].toIdx()], dp[i - 1][alpha.toIdx()] + 2)
                         dp[i][alpha.toIdx()] = minOf(dp[i][alpha.toIdx()], dp[i - 1][alpha.toIdx()] + 2)
                     }
